b94cf1e47401fdd352609a1be4d32942591a7409
Hossein <hbadrnezhad@gmail.com>
2025-11-16 18:46:57 +0330

    session-06

--- main.dart
+++ main.dart
@@ -1,8 +1,14 @@
-import 'dart:io';
-
 void main() {
-  print("hello");
-  stdout.write("Enter your name:");
-  var name = stdin.readLineSync();
-  print("Hello, $name");
+  var name = "Hossein";
+  var age = 31;
+  String field = "Programming";
+  bool isActive = true;
+  double price = 9.99;
+  int count = 5;
+  final country = "Iran";
+  const pi = 3.14159265;
+  late String username;
+  username = "hossein";
+  username = "ali";
+  print("Name: $name, age: $age,field: $field");
 }
