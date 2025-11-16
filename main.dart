61d0208335913b6da347c8a00eaf555d3427d312
Hossein <hbadrnezhad@gmail.com>
2025-11-16 18:47:07 +0330

    session-07

--- main.dart
+++ main.dart
@@ -1,14 +1,63 @@
 void main() {
-  var name = "Hossein";
-  var age = 31;
-  String field = "Programming";
-  bool isActive = true;
-  double price = 9.99;
-  int count = 5;
-  final country = "Iran";
-  const pi = 3.14159265;
-  late String username;
-  username = "hossein";
-  username = "ali";
-  print("Name: $name, age: $age,field: $field");
+  int age = 18;
+
+  if (age >= 18) {
+    print("Adult");
+  } else {
+    print("Young");
+  }
+
+  int score = 85;
+  if (score >= 90) {
+    print("A");
+  } else if (score >= 80) {
+    print("B");
+  } else {
+    print("C");
+  }
+
+  var isLoggedIn = false;
+  var message = isLoggedIn ? "Welcome" : "Please Login";
+  print(message);
+
+  for (var i = 0; i < 5; i++) {
+    print(i);
+  }
+
+  var cars = ["BMW", "Benz", "VW"];
+  for (var car in cars) {
+    print(car);
+  }
+
+  int count = 0;
+  while (count < 3) {
+    print(count);
+    count++;
+  }
+
+  int num = 0;
+  do {
+    if (num == 5) {
+      break;
+    }
+    if (num == 1) {
+      num++;
+      continue;
+    }
+    print("number: $num");
+    num++;
+  } while (num < 10);
+
+  var command = "open";
+
+  switch (command) {
+    case "open":
+      print("Openning File...");
+      break;
+    case "close":
+      print("Closing File...");
+      break;
+    default:
+      print("unknown command");
+  }
 }
