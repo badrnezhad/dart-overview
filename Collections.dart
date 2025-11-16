9744a4bb82000bf7962acd3f7bd2120863a90add
Hossein <hbadrnezhad@gmail.com>
2025-11-16 19:40:31 +0330

    session-13

---
+++ Collections.dart
@@ -0,0 +1,32 @@
+void main() {
+  //list
+  var numbers = [1, 2, 3];
+  print(numbers[0]);
+
+  numbers.add(5);
+  print(numbers);
+
+  numbers.remove(2);
+  numbers.removeAt(2);
+  print(numbers);
+
+  print(numbers.contains(5));
+  numbers.clear();
+  print(numbers);
+
+  //set
+  var fruits = {"apple", "banana", "apple"};
+  print(fruits);
+
+  fruits.add("cherry");
+  fruits.remove("apple");
+  print(fruits);
+
+  //map
+
+  var user = {"name": "Hossein", "age": 31, "isAdmin": true};
+
+  print(user["name"]);
+  user["city"] = "Tehran";
+  
+}
